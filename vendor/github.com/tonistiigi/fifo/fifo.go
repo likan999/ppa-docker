619aae98a6124488dfb7fcbb7aeccf67720849ad
Kan Li <likan999@users.noreply.github.com>
2021-01-03 23:57:57 -0800

    Revert tonistiigi/fifo to old version.
    Summary:
    O_PATH is not supported on 2.6.32 so the implementation is broken.

--- vendor/github.com/tonistiigi/fifo/fifo.go
+++ vendor/github.com/tonistiigi/fifo/fifo.go
@@ -1,6 +1,7 @@
 package fifo
 
 import (
+	"context"
 	"io"
 	"os"
 	"runtime"
@@ -8,20 +9,17 @@ import (
 	"syscall"
 
 	"github.com/pkg/errors"
-	"golang.org/x/net/context"
-	"golang.org/x/sys/unix"
 )
 
 type fifo struct {
-	flag        int
-	opened      chan struct{}
-	closed      chan struct{}
-	closing     chan struct{}
-	err         error
-	file        *os.File
-	closingOnce sync.Once // close has been called
-	closedOnce  sync.Once // fifo is closed
-	handle      *handle
+	path      string
+	flag      int
+	opened    chan struct{}
+	closed    chan struct{}
+	closing   chan struct{}
+	err       error
+	file      *os.File
+	closeOnce sync.Once
 }
 
 var leakCheckWg *sync.WaitGroup
@@ -39,7 +37,7 @@ var leakCheckWg *sync.WaitGroup
 func OpenFifo(ctx context.Context, fn string, flag int, perm os.FileMode) (io.ReadWriteCloser, error) {
 	if _, err := os.Stat(fn); err != nil {
 		if os.IsNotExist(err) && flag&syscall.O_CREAT != 0 {
-			if err := unix.Mkfifo(fn, uint32(perm&os.ModePerm)); err != nil && !os.IsExist(err) {
+			if err := syscall.Mkfifo(fn, uint32(perm&os.ModePerm)); err != nil && !os.IsExist(err) {
 				return nil, errors.Wrapf(err, "error creating fifo %v", fn)
 			}
 		} else {
@@ -52,13 +50,8 @@ func OpenFifo(ctx context.Context, fn string, flag int, perm os.FileMode) (io.Re
 	flag &= ^syscall.O_CREAT
 	flag &= ^syscall.O_NONBLOCK
 
-	h, err := getHandle(fn)
-	if err != nil {
-		return nil, err
-	}
-
 	f := &fifo{
-		handle:  h,
+		path:    fn,
 		flag:    flag,
 		opened:  make(chan struct{}),
 		closed:  make(chan struct{}),
@@ -66,12 +59,10 @@ func OpenFifo(ctx context.Context, fn string, flag int, perm os.FileMode) (io.Re
 	}
 
 	wg := leakCheckWg
-	if wg != nil {
-		wg.Add(2)
-	}
 
 	go func() {
 		if wg != nil {
+			wg.Add(1)
 			defer wg.Done()
 		}
 		select {
@@ -83,13 +74,10 @@ func OpenFifo(ctx context.Context, fn string, flag int, perm os.FileMode) (io.Re
 	}()
 	go func() {
 		if wg != nil {
+			wg.Add(1)
 			defer wg.Done()
 		}
-		var file *os.File
-		fn, err := h.Path()
-		if err == nil {
-			file, err = os.OpenFile(fn, flag, 0)
-		}
+		file, err := os.OpenFile(fn, flag, 0)
 		select {
 		case <-f.closing:
 			if err == nil {
@@ -97,19 +85,15 @@ func OpenFifo(ctx context.Context, fn string, flag int, perm os.FileMode) (io.Re
 				case <-ctx.Done():
 					err = ctx.Err()
 				default:
-					err = errors.Errorf("fifo %v was closed before opening", h.Name())
+					err = errors.Errorf("fifo %v was closed before opening", fn)
 				}
-				if file != nil {
-					file.Close()
-				}
+				file.Close()
 			}
 		default:
 		}
 		if err != nil {
-			f.closedOnce.Do(func() {
-				f.err = err
-				close(f.closed)
-			})
+			f.err = err
+			close(f.closed)
 			return
 		}
 		f.file = file
@@ -143,7 +127,7 @@ func (f *fifo) Read(b []byte) (int, error) {
 	}
 }
 
-// Write from byte array to a fifo.
+// Write from byte array to a buffer.
 func (f *fifo) Write(b []byte) (int, error) {
 	if f.flag&(syscall.O_WRONLY|syscall.O_RDWR) == 0 {
 		return 0, errors.New("writing to read-only fifo")
@@ -163,53 +147,31 @@ func (f *fifo) Write(b []byte) (int, error) {
 
 // Close the fifo. Next reads/writes will error. This method can also be used
 // before open(2) has returned and fifo was never opened.
-func (f *fifo) Close() (retErr error) {
+func (f *fifo) Close() error {
 	for {
 		select {
 		case <-f.closed:
-			f.handle.Close()
-			return
+			return f.err
 		default:
 			select {
 			case <-f.opened:
-				f.closedOnce.Do(func() {
-					retErr = f.file.Close()
-					f.err = retErr
-					close(f.closed)
-				})
+				f.err = f.file.Close()
+				close(f.closed)
 			default:
 				if f.flag&syscall.O_RDWR != 0 {
 					runtime.Gosched()
 					break
 				}
-				f.closingOnce.Do(func() {
+				f.closeOnce.Do(func() {
 					close(f.closing)
 				})
 				reverseMode := syscall.O_WRONLY
 				if f.flag&syscall.O_WRONLY > 0 {
 					reverseMode = syscall.O_RDONLY
 				}
-				fn, err := f.handle.Path()
-				// if Close() is called concurrently(shouldn't) it may cause error
-				// because handle is closed
-				select {
-				case <-f.closed:
-				default:
-					if err != nil {
-						// Path has become invalid. We will leak a goroutine.
-						// This case should not happen in linux.
-						f.closedOnce.Do(func() {
-							f.err = err
-							close(f.closed)
-						})
-						<-f.closed
-						break
-					}
-					f, err := os.OpenFile(fn, reverseMode|syscall.O_NONBLOCK, 0)
-					if err == nil {
-						f.Close()
-					}
-					runtime.Gosched()
+				f, err := os.OpenFile(f.path, reverseMode|syscall.O_NONBLOCK, 0)
+				if err == nil {
+					f.Close()
 				}
 			}
 		}
